56faea828f9c33206980f42d0311af9afc8adeb5
lizzie992 <gulyaskata99@gmail.com>
2024-10-06 23:53:07 +0200

    Fix #14 Names corrected and constants removed

--- Quiz maker/Constants.cs
+++ Quiz maker/Constants.cs
@@ -11,14 +11,12 @@ namespace Quiz_maker
         public const int NUMBER_OF_ALL_ANSWERS = 4;
         public const int NUMBER_OF_CORRECT_ANSWERS = 1; //in case we have multiple correct answers for extra difficulty
         public const int NUMBER_OF_WRONG_ANSWERS = NUMBER_OF_ALL_ANSWERS - NUMBER_OF_CORRECT_ANSWERS;
-        public const string FILEPATH = "..."; //to be added:)
         public const int WIN_POINTS = 3;
         public const int LOSE_POINTS = 1;
-        public const string QUIZMAKER = "q";
+        public const string QUIZ_MAKER = "q";
         public const string PLAY = "p";
         public const string CLOSE = "c";
         public const string PATH = @"..\..\..\..\QuizData.xml";
         public const string NEXT_QUESTION = "n";
-        public const string QUIT_PLAYERMODE = "q";
     }
 }
